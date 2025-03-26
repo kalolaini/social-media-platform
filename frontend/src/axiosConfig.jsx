7d9ea73746b2e245f311abbe0c76de76b601bf05
Kalo <kalolaini.tukana@gmail.com>
2025-03-26 13:31:01 +1000

    BaseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api", 
-  // baseURL: 'http://3.26.96.188:5001/api', 
+  //baseURL: "http://localhost:5001/api", 
+  baseURL: 'http://3.26.39.90/:5001/api', 
   headers: { "Content-Type": "application/json" },
 });
 
